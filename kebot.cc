a07fc9571b6ab1b95192c196550e04ec83f65193
Emil Karlson <jekarlson@gmail.com>
2012-11-06 02:40:30 +0200

    Initial commit.

---
+++ kebot.cc
@@ -0,0 +1,194 @@
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+#include <seccomp.h>
+#include <glib.h>
+#include <v8.h>
+#include <cstdio>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define MAXDATASIZE 1000
+#define SOURCESIZE 409600
+
+using namespace v8;
+
+char buf[MAXDATASIZE];
+
+Handle<Value> XGetter(Local<String> property, 
+	const AccessorInfo& info) {
+	return String::New(buf);
+}
+
+void XSetter(Local<String> property, Local<Value> value,
+	const AccessorInfo& info) {
+}
+
+static Handle<Value> LogCallback(const Arguments& args) {
+	if (args.Length() < 1) return v8::Undefined();
+	HandleScope scope;
+	Handle<Value> arg = args[0];
+	String::Utf8Value value(arg);
+	printf("LOG: %s\n", *value);
+
+	return v8::Undefined();
+}
+
+int s;
+
+int writes(int fd, const char *a) {
+	write(fd, a, strlen(a));
+}
+
+gboolean irc_callback(GIOChannel *source, GIOCondition cond, gpointer ptr)
+{
+	int numbytes = read(s,buf,MAXDATASIZE-1);
+	buf[numbytes]='\0';
+	printf("%s", buf);
+
+	if (strstr(buf,"PING") == buf)
+	{
+		memcpy(buf,"PONG",4);
+		write(s, buf, numbytes);
+	}
+
+	Handle<Script> *script = (Handle<Script>*)ptr;
+
+	// Run the script to get the result.
+	Handle<Value> result = (*script)->Run();
+
+	// Convert the result to an ASCII string and print it.
+	String::AsciiValue ascii(result);
+	printf("ascii %s\n", *ascii);
+	writes(2, *ascii);
+	writes(s, *ascii);
+
+	return TRUE;
+}
+
+char source[SOURCESIZE];
+
+int main()
+{
+	struct addrinfo hints, *servinfo;
+
+	//Ensure that servinfo is clear
+	memset(&hints, 0, sizeof hints); // make sure the struct is empty
+
+	//setup hints
+	hints.ai_family = AF_UNSPEC; // don't care IPv4 or IPv6
+	hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+
+	//Setup the structs if error print why
+	int res;
+	if ((res = getaddrinfo("irc.inet.fi", "6667",&hints,&servinfo)) != 0)
+	{
+		fprintf(stderr,"getaddrinfo: %s\n", gai_strerror(res));
+	}
+
+
+	//setup the socket
+	if ((s = socket(servinfo->ai_family,servinfo->ai_socktype,servinfo->ai_protocol)) == -1)
+	{
+		perror("client: socket");
+	}
+
+	//Connect
+	if (connect(s,servinfo->ai_addr, servinfo->ai_addrlen) == -1)
+	{
+		close (s);
+		perror("Client Connect");
+	}
+
+	memset(source,0,SOURCESIZE);
+	FILE* sourcefp = fopen("kebot.js", "r");
+	fread(source, 1, SOURCESIZE, sourcefp);
+	fclose(sourcefp);
+
+	// Create a stack-allocated handle scope.
+	HandleScope handle_scope;
+
+	// Create a new context.
+	Handle<ObjectTemplate> global = ObjectTemplate::New();
+	global->Set(String::New("log"), FunctionTemplate::New(LogCallback));
+	global->SetAccessor(String::New("x"), XGetter, XSetter);
+	Persistent<Context> context = Context::New(NULL, global);
+
+	// Enter the created context for compiling and
+	// running the hello world script. 
+ 	Context::Scope context_scope(context);
+
+	// Create a string containing the JavaScript source code.
+	Handle<String> sourcehandle = String::New(source);
+
+	// Compile the source code.
+	Handle<Script> script = Script::Compile(sourcehandle);
+
+	//We dont need this anymore
+	freeaddrinfo(servinfo);
+
+	GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
+	GIOChannel *ircchan = g_io_channel_unix_new(s);
+	g_io_add_watch(ircchan, G_IO_IN, irc_callback, &script);
+	
+#ifndef NOSECCOMP
+	int ret = seccomp_init(SCMP_ACT_KILL);
+	if (ret < 0)
+		printf("Error from seccomp_init\n");
+	ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(read), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(write), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(fstat), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(exit), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(poll), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(mmap), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(clone), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(brk), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(mprotect), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(futex), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(nanosleep), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(munmap), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(gettid), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(set_robust_list), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ERRNO(5), SCMP_SYS(open), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(exit_group), 0);
+	if (!ret)
+		ret = seccomp_rule_add(SCMP_ACT_ALLOW, SCMP_SYS(prctl), 0);
+	if (!ret)
+		ret = seccomp_load();
+	if (ret)
+		printf("error setting seccomp\n");
+#endif
+
+	writes(s, "USER MyRealName * * :My Description\n");
+	writes(s, "NICK Lipschitz-test\n");
+
+	g_main_loop_run(main_loop);
+	
+	// Dispose the persistent context.
+	context.Dispose();
+}
+
---
+++ kebot.js
@@ -0,0 +1,38 @@
+var msgre = /:(\S+) PRIVMSG (\S+) :(.+)/i
+var joinre = /:(\S+) JOIN :(\S+)/i
+var cmdre = /%(\S+)( +\S.+)?/i
+
+var lines = x.split("\n")
+
+function cmdevent(command, parameters){
+	log(command)
+	if ("join" == command)
+		return 'JOIN ' + parameters + '\n';
+	return "";
+}
+
+function msgevent(who,whom,message){
+	log(message)
+	log(who)
+	log(whom)
+	if (who == 'Ke!jkarlson@lyta.org.aalto.fi') {
+		var cmd = cmdre.exec(message)
+		if (cmd) {
+			return cmdevent(cmd[1],cmd[2])
+		}
+	}
+	return ""
+}
+
+function f(b){
+	var retval=''
+	for (i in b) {
+		var msg = msgre.exec(b[i])
+		if (msg)
+			retval = msgevent(msg[1],msg[2],msg[3]) 
+	}
+	return retval
+}
+
+f(lines)
+
